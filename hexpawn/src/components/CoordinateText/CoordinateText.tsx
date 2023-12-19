e73ad0ef1424773780aeed1676dd6dfceacae136
LeeJonesDev <leeastonjones.development@gmail.com>
2023-12-19 03:08:33 -0600

    Fix export

--- hexpawn/src/components/CoordinateText/CoordinateText.tsx
+++ hexpawn/src/components/CoordinateText/CoordinateText.tsx
@@ -1,10 +1,10 @@
 import { CoordinateTextProps } from "./CoordinateTextProps"
 
 const CoordinateText = ({ text, ...props }: CoordinateTextProps) => {
-    const CoordiateText = new SVGTextElement()
-
     return <svg
         {...props}>
         {text}
     </svg>
 }
+
+export default CoordinateText
